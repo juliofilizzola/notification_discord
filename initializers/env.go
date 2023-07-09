13082cf9f1faf6f6e66538901fcfca434a5cbaf7
julio_filizzola <juliofilizzola@hotmail.com>
2023-07-09 16:22:49 -0300

    feat: load env create

---
+++ initializers/env.go
@@ -0,0 +1,32 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	Port         = ""
+	TokenDiscord = ""
+	WebhookId    = ""
+	WebhookToken = ""
+	AvatarURL    = ""
+	Username     = ""
+)
+
+func Env() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	Port = fmt.Sprint(os.Getenv("PORT"))
+	TokenDiscord = fmt.Sprint(os.Getenv("TOKEN_DISCORD"))
+	WebhookId = fmt.Sprint(os.Getenv("WEBHOOK_ID"))
+	WebhookToken = fmt.Sprint(os.Getenv("WEBHOOK_TOKEN"))
+	Username = fmt.Sprint(os.Getenv("USERNAME"))
+	AvatarURL = fmt.Sprint(os.Getenv("AVATAR_URL"))
+}
