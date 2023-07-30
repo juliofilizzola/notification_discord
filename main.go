85b64b7b442a9d9e1e699fa3632f6fb02a47b3ff
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-07-29 21:21:22 -0300

    feat: config main initialize dependencies

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
-	"github.com/juliofilizzola/bot_discord/controller"
-	"github.com/juliofilizzola/bot_discord/initializers"
+	"github.com/google/logger"
+	"github.com/juliofilizzola/bot_discord/adpter/input/controller"
+	"github.com/juliofilizzola/bot_discord/adpter/input/controller/routes"
+	"github.com/juliofilizzola/bot_discord/app/services"
+	discord2 "github.com/juliofilizzola/bot_discord/config/discord"
+	"github.com/juliofilizzola/bot_discord/config/env"
 )
 
 func init() {
-	initializers.Env()
+	env.Env()
 }
 
 func main() {
+	logger.Info("init service")
 	r := gin.Default()
-	r.POST("/:id/:token", controller.GithubController)
-	r.Run()
+
+	webController := initDependencies()
+	routes.InitRoutes(&r.RouterGroup, webController)
+
+	if err := r.Run(env.Port); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initDependencies() controller.WebhookControllerInterface {
+	discord, err := discord2.StartDiscord()
+	if err != nil {
+		log.Fatal(err)
+	}
+	service := services.NewWebhookDomainService(discord)
+	return controller.NewWebhookControllerInterface(service)
 }
