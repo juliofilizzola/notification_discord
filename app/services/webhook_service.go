066081e72687d2926c4c3cdea942845446e505ee
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-07-29 21:20:52 -0300

    feat: create service webhook

---
+++ app/services/webhook_service.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/juliofilizzola/bot_discord/app/port/input"
+)
+
+func NewWebhookDomainService(discord *discordgo.Session) input.WebhookDomainService {
+	return &webhookDomainService{
+		server: discord,
+	}
+}
+
+type webhookDomainService struct {
+	server *discordgo.Session
+}
+
+func (web webhookDomainService) Send(dataGit *discordgo.WebhookParams, webhookId, webhookToken, action string) string {
+	if action == "labeled" {
+		webhook, err := web.server.WebhookExecute(webhookId, webhookToken, false, dataGit)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(webhook)
+
+		return "deu bom"
+	}
+	return "deu ruim!"
+}
