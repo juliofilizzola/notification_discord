b090429eb8440c5304357a6c758ebea2715b7397
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-07-30 21:04:32 -0300

    [feat]: remove app dir (#14)
    * feat: update gin
    
    * feat: remove dir internal
    
    * feat: add dependencies logger
    
    * feat: create routes
    
    * feat: create interface webhook domain
    
    * feat: create controller
    
    * feat: create github domain
    
    * feat: create convert domain from send from webhook discord
    
    * feat: config discord (`initializer bot discord`)
    
    * feat: remove var from env
    
    * feat: create service webhook
    
    * feat: config main initialize dependencies
    
    * feat: config main initialize dependencies
    
    * feat: remove logger
    
    * feat: remove app

--- app/port/input/webhook.go
+++
@@ -1,9 +0,0 @@
-package input
-
-import (
-	"github.com/bwmarrin/discordgo"
-)
-
-type WebhookDomainService interface {
-	Send(dataGit *discordgo.WebhookParams, webhookId, webhookToken, action string) string
-}
